d5e8a78da9ddffbd43f95ddf46f4a44cf0b184c9
iainharfield <iain.harfield@ntlworld.com>
2022-11-30 23:14:24 +0000

    fixes

--- src/asyncConnect.cpp
+++ src/asyncConnect.cpp
@@ -1,13 +1,5 @@
 /****************************************************************************************************************************
-  FullyFeatureSSL_ESP8266.ino 2022
 
-  AsyncMqttClient_Generic is a library for ESP32, ESP8266, Protenta_H7, STM32F7, etc. with current AsyncTCP support
-
-  Based on and modified from :
-
-  1) async-mqtt-client (https://github.com/marvinroger/async-mqtt-client)
-
-  Built by Khoi Hoang https://github.com/khoih-prog/AsyncMqttClient_Generic
  *****************************************************************************************************************************/
 
 #include <ESP8266WiFi.h>
